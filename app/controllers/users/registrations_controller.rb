511e4ab1c7b5eaa5d84f8f180cd21a1431f994c4
karajan-37458 <66232530+karajan-37458@users.noreply.github.com>
2020-07-02 18:06:59 +0900

    CreateUsersFunction5

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,7 @@ class Users
   class RegistrationsController < Devise::RegistrationsController
     # before_action :configure_permitted_parameters, only: [:create]
     # before_action :configure_account_update_params, only: [:update]
-  
+
     # GET /resource/sign_up
     def new
       @user = User.new
