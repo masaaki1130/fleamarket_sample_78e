ab38b548a916a722d431ba1945cc3b746de78b37
masaaki1130 <masaki11maki30@gmail.com>
2020-07-02 11:28:02 +0900

    products_edit3

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,5 +11,5 @@ class ProductsController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:name, :image, :text, :category_id, :brand_id, :status, :charges, :price, :postage, :area, :day, :user_id)
-end
+  end
 end
