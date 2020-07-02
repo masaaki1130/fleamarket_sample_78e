22421ac8d55c81c06ff5669f238472f9f0671f39
karajan-37458 <66232530+karajan-37458@users.noreply.github.com>
2020-07-02 18:04:52 +0900

    CreateUsersFunction3

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_permitted_parameters, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
-
-  # GET /resource/sign_up
-  def new
-    @user = User.new
+class Users
+  class RegistrationsController < Devise::RegistrationsController
+    # before_action :configure_permitted_parameters, only: [:create]
+    # before_action :configure_account_update_params, only: [:update]
+  
+    # GET /resource/sign_up
+    def new
+      @user = User.new
+    end
   end
 end
