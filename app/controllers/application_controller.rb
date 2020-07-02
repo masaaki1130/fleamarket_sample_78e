38da27d4aadb424fb6f734885df79e7420cf1203
karajan-37458 <66232530+karajan-37458@users.noreply.github.com>
2020-07-02 17:08:03 +0900

    CreateUsersFunction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def production?
@@ -10,5 +11,10 @@ class ApplicationController < ActionController::Base
       username == Rails.application.credentials[:basic_auth][:user] &&
       password == Rails.application.credentials[:basic_auth][:pass]
     end
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :first_name_kana, :family_name_kana, :birth_day])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
   root 'items#index'
-  devise_for :users
   resources :products, only: [:new, :create]
+  resources :users
 end
