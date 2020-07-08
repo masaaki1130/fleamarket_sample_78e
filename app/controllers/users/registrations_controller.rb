62f104b23b13d07d7d9b48aee65615082fc7a6ba
karajan-37458 <66232530+karajan-37458@users.noreply.github.com>
2020-07-08 19:03:30 +0900

    AddAddressRegistrationFunction

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,5 +7,36 @@ module Users
     def new
       @user = User.new
     end
+
+    def create
+      @user = User.new(sign_up_params)
+    unless @user.valid?
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @address = @user.addresses.new
+    render :new_destinations
+    end
+
+    def create_destinations
+      @user = User.new(session["devise.regist_data"]["user"])
+      @address = @user.addresses.new(destination_params)
+      unless @address.valid?
+        flash.now[:alert] = @address.errors.full_messages
+        render :new_destinations and return
+      end
+      @user.save
+      @address.save
+      session["devise.regist_data"]["user"].clear
+      sign_in(:user, @user)
+    end
+
+    protected
+
+    def destination_params
+      params.require(:address).permit(:family_name, :given_name, :family_name_kana, :given_name_kana, :postal, :prefecture_id, :city, :house_number, :building_name, :phone_number)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'destinations', to: 'users/registrations#new_destinations'
+    post 'destinations', to: 'users/registrations#create_destinations'
+  end
+
   root 'items#index'
   resources :products, only: [:new, :create]
-  resources :users, only: :show
+  resources :user
   resources :card, only: :index
   resources :mypage
   resources :logout, only: :index
