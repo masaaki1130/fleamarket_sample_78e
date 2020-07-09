202d8a6e558494a0c3f0055cf743aaac1ceab35d
masaaki1130 <masaki11maki30@gmail.com>
2020-07-09 17:43:13 +0900

    fix5

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,9 @@ Rails.application.routes.draw do
     get 'search'
     get 'category/get_category_children', to: 'products#get_category_children', defaults: { format: 'json' }
     get 'category/get_category_grandchildren', to: 'products#get_category_grandchildren', defaults: { format: 'json' }
-
-  end
   end
 end
+
 
   resources :user
   resources :card, only: :index
