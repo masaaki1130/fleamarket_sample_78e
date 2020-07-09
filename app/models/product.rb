2b7df963134773cc1c5b99b777e3842305f7d055
masaaki1130 <66311435+masaaki1130@users.noreply.github.com>
2020-07-09 19:38:59 +0900

    Revert "Implementation-of-listing-function"

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :user
-  belongs_to :category
+  belongs_to :category, optional: true
   belongs_to :brand
   belongs_to :status
   belongs_to :day
