532ca70f359251c8b1f27f9b9e97aef5b32f891f
karajan-37458 <66232530+karajan-37458@users.noreply.github.com>
2020-07-09 20:07:45 +0900

    FixProducts.js

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,4 +1,20 @@
 $(document).on('turbolinks:load', ()=> {
+  $(function(){
+    $('#price_calc').on('input', function(){   //リアルタイムで表示したいのでinputを使う｡入力の度にイベントが発火するようになる｡
+      var data = $('#price_calc').val(); // val()でフォームのvalueを取得(数値)
+      var profit = Math.round(data * 0.9)  // 手数料計算を行う｡dataにかけているのが0.9なのは単に引きたい手数料が10%のため｡
+      var fee = (data - profit) // 入力した数値から計算結果(profit)を引く｡それが手数料となる｡
+      $('.main-contents__price-fee__right-bar').html(fee) //  手数料の表示｡html()は追加ではなく､上書き｡入力値が変わる度に表示も変わるようにする｡
+      $('.main-contents__price-fee__right-bar').prepend('¥') // 手数料の前に¥マークを付けたいので
+      $('.main-contents__price__profit-calculation__right-bar').html(profit)
+      $('.main-contents__price__profit-calculation__right-bar').prepend('¥')
+      $('#price').val(profit) // 計算結果を格納用フォームに追加｡もし､入力値を追加したいのなら､今回はdataを引数に持たせる｡
+      if(profit == '') {   // もし､計算結果が''なら表示も消す｡
+      $('.main-contents__price__profit-calculation__right-bar').html('');
+      $('.main-contents__price-fee__right-bar').html('');
+      }
+    })
+  })
   // 画像用のinputを生成する関数
   const buildFileField = (index)=> {
     const html = `<div data-index="${index}" class="main-contents__image__photo">
@@ -57,19 +73,4 @@ $(document).on('turbolinks:load', ()=> {
   });
 });
 
-$(function(){
-  $('#price_calc').on('input', function(){   //リアルタイムで表示したいのでinputを使う｡入力の度にイベントが発火するようになる｡
-    var data = $('#price_calc').val(); // val()でフォームのvalueを取得(数値)
-    var profit = Math.round(data * 0.9)  // 手数料計算を行う｡dataにかけているのが0.9なのは単に引きたい手数料が10%のため｡
-    var fee = (data - profit) // 入力した数値から計算結果(profit)を引く｡それが手数料となる｡
-    $('.main-contents__price-fee__right-bar').html(fee) //  手数料の表示｡html()は追加ではなく､上書き｡入力値が変わる度に表示も変わるようにする｡
-    $('.main-contents__price-fee__right-bar').prepend('¥') // 手数料の前に¥マークを付けたいので
-    $('.main-contents__price__profit-calculation__right-bar').html(profit)
-    $('.main-contents__price__profit-calculation__right-bar').prepend('¥')
-    $('#price').val(profit) // 計算結果を格納用フォームに追加｡もし､入力値を追加したいのなら､今回はdataを引数に持たせる｡
-    if(profit == '') {   // もし､計算結果が''なら表示も消す｡
-    $('.main-contents__price__profit-calculation__right-bar').html('');
-    $('.main-contents__price-fee__right-bar').html('');
-    }
-  })
-})
+
