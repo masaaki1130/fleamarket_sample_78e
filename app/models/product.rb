c67f21e8a5e6b85e8810ca09a1b6cb09bee8387e
masaaki1130 <masaki11maki30@gmail.com>
2020-07-14 12:20:53 +0900

    updatedestroy2

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   belongs_to_active_hash :day
   belongs_to_active_hash :shipping_cost
   has_many :purchases
-  has_many :images, inverse_of: :product,dependent: :destroy
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :user
   belongs_to :category
