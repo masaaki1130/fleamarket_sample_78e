f4372576bdc5e69abf1a8c6f34b17927b7b09987
karajan-37458 <66232530+karajan-37458@users.noreply.github.com>
2020-07-16 18:46:13 +0900

    Fix Rubocop's error

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -68,11 +68,10 @@ class ProductsController < ApplicationController
 
   def buy_confirmation
     @product = Product.find(params[:id])
-    if @card.present?
+    return unless @card.present?
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @default_card_information = customer.cards.retrieve(@card.card_id)
-    end
   end
 
   private
