3c853c805753ad1040d8427e4f9ec0a6d60ee68a
masaaki1130 <masaki11maki30@gmail.com>
2020-07-16 16:28:14 +0900

    updatepan2

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,38 +1,31 @@
-#ルート
 crumb :root do
   link "トップページ", root_path
 end
 
-#マイページ
 crumb :mypage do
   link "マイページ", mypage_index_path
 end
 
-# 会員情報入力ページ(1ページ)
 crumb :users_new do
   link "新規会員登録", new_user_registration_path
   parent :root
 end
 
-#クレジットカード登録
 crumb :card do
   link "クレジットカード", new_card_path
   parent :mypage
 end
 
-# ログイン
 crumb :login do
   link "ログイン", new_user_session_path
   parent :root
 end
 
-# 商品詳細ページ
 crumb :products_show do
   link "商品詳細"
   parent :root
 end
 
-# 出品ページ
 crumb :products_new do
   link "出品ページ"
   parent :root
